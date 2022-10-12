5800f636b9a48e53c3bbc09705dc774a55a6cfdb
Nganga Kingori <ngangakelvin254@gmail.com>
2022-10-12 09:23:20 +0300

    Hybrid ms

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
-    transport: Transport.REDIS
+    transport: Transport.REDIS,
   })
   await app.startAllMicroservices();
   await app.listen(3000);
