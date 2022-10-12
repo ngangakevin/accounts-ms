cbb1d5aa0d700fe8c2eaa4cd9ca8b44db99496a8
Nganga Kingori <ngangakelvin254@gmail.com>
2022-10-12 09:23:04 +0300

    define hybrid (Rest + REDIS ms) model.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.REDIS
+  })
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
