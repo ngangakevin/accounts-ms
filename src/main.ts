12607d5110062d0ae1673c3fea471db85aeff6c7
Nganga Kingori <ngangakelvin254@gmail.com>
2022-10-23 11:51:39 +0300

    hybrid microservice connection

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice({
     transport: Transport.REDIS,
+    options: {
+      port: parseInt(process.env.REDIS_PORT),
+      host: process.env.REDIS_HOST,
+      retryAttempts: parseInt(process.env.REDIS_CONNECTION_RETRY),
+      retryDelay: parseInt(process.env.REDIS_CONNECTION_RETRY_BACKOFF),
+    }
   })
   await app.startAllMicroservices();
   await app.listen(3000);
