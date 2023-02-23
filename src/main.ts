5fb4d278718b44367ab6e2ca8855b1ddd4e12702
Nganga Kingori <ngangakelvin254@gmail.com>
2023-02-23 15:10:13 +0300

    swagger-api setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConfigModule } from './configs/config.module';
 import { ConfigService } from './configs/config.service';
@@ -10,6 +11,17 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log'],
   });
   const configs = app.select(ConfigModule).get(ConfigService);
+  const swaggerConfigs = new DocumentBuilder()
+    .setTitle('Accounts Service')
+    .setDescription(
+      'Service responsible for all acocunts mutations' +
+        ' including creation, deposits, transfers, freeze and unfreezing.',
+    )
+    .setVersion('1.0')
+    .addTag('accounts')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfigs);
+  SwaggerModule.setup('accounts/api', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
